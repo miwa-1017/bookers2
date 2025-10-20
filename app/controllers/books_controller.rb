0ab3066b1aaaf41ddee6a1240cec1481c3d05fdc
miwa-1017 <ubuntu@ip-172-31-28-251.ap-northeast-1.compute.internal>
2025-10-20 09:36:41 +0000

    Bookers2課題完成版

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,4 @@
 class BooksController < ApplicationController
-  before_action :authenticate_user!
 
   def index
     @books = Book.all
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,35 +1,42 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!,
-  except: [:index]
-  def show
-    @user = User.find(params[:id])
-    @books = @user.books
-    @book = Book.new
+  before_action :is_matching_login_user, only: [:edit, :update]
+    def show
+      @user = User.find(params[:id])
+      @books = @user.books
+      @book = Book.new
+    end
+
+    def index
+      @users = User.all
+      @book = Book.new
+      @user = current_user
+    end
+
+    def edit
+      @user = current_user
+    end
+
+    def update
+      @user = current_user
+      if @user.update(user_params)
+        redirect_to user_path(@user), notice: "Update was successfully"
+      else
+        render :edit
+      end
+    end
+
+  private
+
+  def user_params
+
+  params.require(:user).permit(:name, :introduction, :profile_image)
   end
 
-  def index
-    @users = User.all
-    @book = Book.new
-    @user = current_user
-  end
-
-  def edit
-    @user = current_user
-  end
-
-  def update
-    @user = current_user
-    if @user.update(user_params)
-      redirect_to user_path(@user), notice: "Update was successfully"
-    else
-      render :edit
+  def is_matching_login_user
+    user_id = params[:id].to_i
+    login_user_id = current_user.id
+    if user_id != login_user_id
+      redirect_to user_path(login_user_id)
     end
   end
-
- private
-
- def user_params
-
-params.require(:user).permit(:name, :introduction, :profile_image)
- end
 end
